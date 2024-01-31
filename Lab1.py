ffeea68d3d990c66f2d5684d241efbaa122aaf20
evansfsu <eschneider@sfsu.edu>
2024-01-31 15:57:09 -0800

    Create Lab1.py

---
+++ Lab1.py
@@ -0,0 +1,10 @@
+"""
+Author: Evan Schneider
+File name: Lab1.py
+Description: Lab1 Tutorial
+Date: 1/31/2024
+"""
+
+message = "Go Gators" #message variable declared as containing "Go Gators" string.
+
+print(message) #prints the variable, which prints the string.
