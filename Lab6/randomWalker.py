4013f5c2e798f9a3bf7ef817cdb5f46281c24477
EvanSneuro <153248801+EvanSneuro@users.noreply.github.com>
2024-03-10 16:38:41 -0700

    Lab6 Commit
    lab6

---
+++ Lab6/randomWalker.py
@@ -0,0 +1,89 @@
+"""
+Modified by Evan Schneider
+
+Assignment adapted from HMC CS5
+"""
+
+import random  
+import time
+
+def rs():
+    """rs chooses a random step and returns it.
+       Note that a call to rs() requires parentheses.
+       Arguments: none at all!
+    """
+    return random.choice([-1, 1])
+
+def rwpos(start, nsteps):
+    """ rwpos models a random walker that
+        * starts at the int argument, start
+        * takes the int # of steps, nsteps
+
+        rwpos returns the final position of the walker.
+    """
+    time.sleep(0.1)
+    print('location is', start)
+    if nsteps == 0:
+        return start
+    else:
+        newpos = start + rs()  # take one step
+        return rwpos(newpos, nsteps - 1)
+
+def rwsteps(start, low, hi):
+    """ rwsteps models a random walker which
+        * is currently at start 
+        * is in a walkway from low (usually 0) to hi (max location) 
+          
+        rwsteps returns the # of steps taken 
+        when the walker reaches an edge
+    """
+    walkway = "_"*(hi-low)    # create a walkway of underscores
+    S = (start-low)           # this is our sleepwalker's location, start-low
+
+    walkway = walkway[:S] + "S" + walkway[S:]  # put our sleepwalker, "S", there
+
+    walkway = " " + walkway + " "              # surround with spaces, for now...
+
+    print(walkway, "    ", start, low, hi)     # print everything to keep track...
+    time.sleep(0.05)
+
+    if start <= low or start >= hi:            # base case: no steps if we're at an endpt
+        return 0
+    
+    else:
+        newstart = start + rs()                # takes one step, from start to newstart
+        return 1 + rwsteps(newstart, low, hi)  # counts one step, recurses for the rest!
+
+def rwstepsLoop(start, low, hi):
+    """ rwstepsLoop models a random walker which
+        * is currently at start 
+        * is in a walkway from low (usually 0) to hi (max location) 
+          
+        rwstepsLoop returns the # of steps taken 
+        when the walker reaches an edge using a loop.
+    """
+    walkway = "_" * (hi - low)    # create a walkway of underscores
+    S = (start - low)             # this is our sleepwalker's location, start-low
+
+    walkway = walkway[:S] + "S" + walkway[S:]  # put our sleepwalker, "S", there
+
+    walkway = " " + walkway + " "              # surround with spaces, for now...
+
+    print(walkway, "    ", start, low, hi)     # print everything to keep track...
+    time.sleep(0.05)
+
+    steps = 0
+    while start > low and start < hi:  # loop until the walker reaches an endpt
+        start += rs()                  # take one step
+        steps += 1                     # increment step count
+        walkway = "_" * (hi - low)     # recreate the walkway for printing
+        S = (start - low)
+        walkway = walkway[:S] + "S" + walkway[S:]  
+        walkway = " " + walkway + " "
+        print(walkway, "    ", start, low, hi)
+        time.sleep(0.05)
+
+    return steps
+
+if __name__ == '__main__':
+    print(rs())
